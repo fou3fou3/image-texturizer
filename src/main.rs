d6bca61cbfe6ee924ae322f372501c951eb7b62a
fou3fou3 <foxedex29@gmail.com>
2024-12-27 15:09:10 +0100

    fix aspect ratio relative to the terminal && update gitignore

--- src/main.rs
+++ src/main.rs
@@ -16,7 +16,7 @@ fn gray_scale_image(input_image: DynamicImage, c: f32) -> ImageBuffer<Luma<u8>, 
 }
 
 fn resize_image(input_image: DynamicImage, h: u32, w: u32) -> DynamicImage {
-    input_image.resize(w, h, FilterType::Lanczos3)
+    input_image.resize_exact(w, h, FilterType::Lanczos3)
 }
 
 fn print_assci_on_intensity(intensity: u8) {
@@ -36,7 +36,7 @@ fn main() {
 
     let img = image::open(args.image_path).unwrap();
     let down_scaled_height = (img.height() * args.descale_rate) / 100;
-    let down_scaled_width = (img.width() * args.descale_rate) / 100;
+    let down_scaled_width = (img.width() * args.descale_rate) / 50; // divide by 50 because hight = 2 x width, (in terminal)
 
     let resized_img = resize_image(img, down_scaled_height, down_scaled_width);
     let gray_scaled_img = gray_scale_image(resized_img, 20.0);
